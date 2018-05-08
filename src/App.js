dc4ab4119c580b0fd478cf787c4a2a929f2da2d9
Steffen Buchmann <steffen.buchmann@canda.com>
2018-05-08 13:39:58 +0200

    removed console.log

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class BooksApp extends React.Component {
   componentDidMount() {
     BooksAPI.getAll()
       .then((books) => {
-        console.log('XXX', books);
         this.setState(() => ({
             books
         }))
